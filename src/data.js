eee4fbaf594c0008538555d3b4931f22e7000f7c
remiyeku <antakin4@gmail.com>
2024-01-31 16:05:43 +0000

    added some project data

---
+++ src/data.js
@@ -0,0 +1,48 @@
+export const projects = [
+  {
+    title: "Cheers",
+    subtitle: "Bar Finding App",
+    description:
+      "For the final 4-week project of the SoC bootcamp our team took a problem-first approach to ideation: We decided to create an app that solves the problem of needing to visit several websites and do a frustratingly large number of google searches to plan a night out - and still not finding all the information required to be sure a bar is right for you! Using different filters and search capabilities including a map feature, Cheers users have all the information they could possibly desire at their fingertips - no matter the group, occasion or features required.",
+    image: "/public/images/cheers-hp.png",
+    link: "https://cheersy.vercel.app/",
+    gitlink:
+      "https://github.com/SchoolOfCode/bc13_final-project_front-end-beast-code",
+  },
+  {
+    title: "Pokemon randomiser",
+    subtitle: "A Pokemon Randomiser",
+    description:
+      "A Pokemon randomiser, with pookemon!! (What's not to love). Collecting data from the pokemonapi and rendering it on screen. Built in react. I have plans to use this api again to create a battle game!",
+    image: "/public/images/pokelogo.png",
+    link: "https://pokemon-randomiser-62jmzj3i0-remiyeku.vercel.app",
+    gitlink: "https://github.com/remiyeku/Pokemon-Randomiser",
+  },
+  {
+    title: "Naughts and Crosses",
+    subtitle: "CSS flex practice",
+    description:
+      "A simple naughts and crosses game built in react, showcasing many css techniques such as flexbox and grids. The page built in React and is fully responsive.",
+    image: "/public/images/n and c.JPG",
+    link: "https://react-naughts-and-crosses-uut8.vercel.app/",
+    gitlink: "https://github.com/remiyeku/react-naughts-and-crosses",
+  },
+  {
+    title: "Simpsons Quote Randomiser",
+    subtitle: "A Simpsons Randomiser",
+    description:
+      "A Simpsons randomiser, collecting data from a free simpsons quote api and rendering it on screen. Built in React. ",
+    image: "/public/images/simpsy.png",
+    link: "https://simpsons-five.vercel.app/",
+    gitlink: "https://github.com/remiyeku/simpsons",
+  },
+  {
+    title: "Weather App",
+    subtitle: "A Current Weather and Forecast App",
+    description:
+      "A weather app built within React, fetching data from a geo cities and weather api concurrently to display the current weather and forecast of every city in the database. A city can be searched by a user and it will show comprehensive weather data. App is fully responsive.",
+    image: "/public/images/simpsy.png",
+    link: "https://weather-blush-pi.vercel.app/",
+    gitlink: "https://github.com/remiyeku/weather",
+  },
+];
